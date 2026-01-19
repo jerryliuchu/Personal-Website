d92f3d4ab4ea5b18d1b9c09d070aabfa83d088dd
Jerry Chu <mlgerrydoge@gmail.com>
2026-01-19 23:49:06 +0000

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  base: 'jerryliuchu.github.io/repo-name/',
+  base: '/Personal-Website/',
   
   plugins: [
     react(),
